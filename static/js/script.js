6c27ae6250998ebee53173e82e2441925af40a57
gerwini <gerwinjb@hotmail.nl>
2020-11-14 20:08:45 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.tooltipped').tooltip();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
   });
